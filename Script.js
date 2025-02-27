47ef9e3fc3d98ea47a5daddf0d1d010f42e3e815
Mangga <anggaprima2006@gmail.com>
2025-02-27 20:52:02 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,16 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const boxes = document.querySelectorAll(".box");
     let index = 0;
 
     function showBoxes() {
         // Sembunyikan semua box dengan efek fade out
-        boxes.forEach(box => {
+        boxes.forEach((box) => {
             box.classList.remove("active");
         });
 
-        // Tampilkan 2 box saat ini dengan efek fade in
+        // Tampilkan 2 box dengan efek fade in
         boxes[index].classList.add("active");
         boxes[(index + 1) % boxes.length].classList.add("active");
+
+        // Scroll animasi ke box yang sedang tampil
+        document.querySelector(".scroll-container").scrollTo({
+            left: boxes[index].offsetLeft - 50,
+            behavior: "smooth",
+        });
 
         // Update index untuk iterasi selanjutnya
         index = (index + 1) % boxes.length;
@@ -19,6 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // Tampilkan 2 box pertama saat halaman dimuat
     showBoxes();
 
-    // Ubah tampilan setiap 3 detik
+    // Jalankan efek setiap 3 detik
     setInterval(showBoxes, 3000);
 });
